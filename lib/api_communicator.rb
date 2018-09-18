057eebbb72793e952e2faf723ab07ae47de26c88
Hanaa Sadoqi <hsadoqi@gmail.com>
2018-09-18 18:32:25 +0000

    automatically backed up by learn

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -7,7 +7,12 @@ def get_character_movies_from_api(character)
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
   
-  
+  films_array = response_hash["results"][0]["films"]
+
+  films_array.each do |film|
+    film_info = RestClient.get(film)
+    film_json = JSON.parse(film_info)
+  end
   
   # NOTE: in this demonstration we name many of the variables _hash or _array. 
   # This is done for educational purposes. This is not typically done in code.
